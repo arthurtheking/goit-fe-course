864b321040b1f93a5c57bf4ecbe09b811638762e
Arthur <bardartur@gmail.com>
2019-01-26 11:39:50 +0200

    upd 4

--- js-intense/module-04/js/scripts.js
+++ js-intense/module-04/js/scripts.js
@@ -52,13 +52,13 @@ const notepad = {
         const arr = [];
         for(let note of this.notes) {               //WORKS
             
-            if(note.title.toLowerCase().includes(query) || note.body.toLowerCase().includes(query) || note.title.toUpperCase().includes(query) || note.body.toUpperCase().includes(query)) {      //WORKS
+            if(note.title.toLowerCase().includes(query.toLowerCase()) || note.body.toLowerCase().includes(query.toLowerCase())) {      //WORKS
                 arr.push(note);
             } else {
             
             }
         };
-        return arr.length > 0 ? arr : null;
+        return arr
 
     },
 
@@ -77,94 +77,94 @@ const notepad = {
 /*
  * Добавляю 4 заметки и смотрю что получилось
  */
-notepad.saveNote({
-    id: 1,
-    title: 'JavaScript essentials',
-    body:
-      'Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc',
-    priority: PRIORITY_TYPES.HIGH,
-  });
+// notepad.saveNote({
+//     id: 1,
+//     title: 'JavaScript essentials',
+//     body:
+//       'Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc',
+//     priority: PRIORITY_TYPES.HIGH,
+//   });
   
-  notepad.saveNote({
-    id: 2,
-    title: 'Refresh HTML and CSS',
-    body:
-      'Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.',
-    priority: PRIORITY_TYPES.NORMAL,
-  });
+//   notepad.saveNote({
+//     id: 2,
+//     title: 'Refresh HTML and CSS',
+//     body:
+//       'Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.',
+//     priority: PRIORITY_TYPES.NORMAL,
+//   });
   
-  notepad.saveNote({
-    id: 3,
-    title: 'Get comfy with Frontend frameworks',
-    body:
-      'First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.',
-    priority: PRIORITY_TYPES.NORMAL,
-  });
+//   notepad.saveNote({
+//     id: 3,
+//     title: 'Get comfy with Frontend frameworks',
+//     body:
+//       'First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.',
+//     priority: PRIORITY_TYPES.NORMAL,
+//   });
   
-  notepad.saveNote({
-    id: 4,
-    title: 'Winter clothes',
-    body:
-      "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
-    priority: PRIORITY_TYPES.LOW,
-  });
+//   notepad.saveNote({
+//     id: 4,
+//     title: 'Winter clothes',
+//     body:
+//       "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
+//     priority: PRIORITY_TYPES.LOW,
+//   });
   
-  console.log('Все текущие заметки: ', notepad.getNotes());
+//   console.log('Все текущие заметки: ', notepad.getNotes());
   
-  /*
-   * Зима уже близко, пора поднять приоритет на покупку одежды
-   */
-  notepad.updateNotePriority(4, PRIORITY_TYPES.NORMAL);
-  // Смотрю что у меня в заметках
-  console.log(
-    'Заметки после обновления приоритета для id 4: ',
-    notepad.getNotes(),
-  );
+//   /*
+//    * Зима уже близко, пора поднять приоритет на покупку одежды
+//    */
+//   notepad.updateNotePriority(4, PRIORITY_TYPES.NORMAL);
+//   // Смотрю что у меня в заметках
+//   console.log(
+//     'Заметки после обновления приоритета для id 4: ',
+//     notepad.getNotes(),
+//   );
   
-  /*
-   * Решил что фреймворки отложу немного, понижаю приоритет
-   */
-  notepad.updateNotePriority(3, PRIORITY_TYPES.LOW);
-  console.log(
-    'Заметки после обновления приоритета для id 3: ',
-    notepad.getNotes(),
-  );
+//   /*
+//    * Решил что фреймворки отложу немного, понижаю приоритет
+//    */
+//   notepad.updateNotePriority(3, PRIORITY_TYPES.LOW);
+//   console.log(
+//     'Заметки после обновления приоритета для id 3: ',
+//     notepad.getNotes(),
+//   );
   
-  /*
-   * Решил отфильтровать заметки по слову html
-   */
-  console.log(
-    'Отфильтровали заметки по ключевому слову "html": ',
-    notepad.filterNotesByQuery('html'),
-  );
+//   /*
+//    * Решил отфильтровать заметки по слову html
+//    */
+//   console.log(
+//     'Отфильтровали заметки по ключевому слову "html": ',
+//     notepad.filterNotesByQuery('html'),
+//   );
   
-  /*
-   * Решил отфильтровать заметки по слову javascript
-   */
-  console.log(
-    'Отфильтровали заметки по ключевому слову "javascript": ',
-    notepad.filterNotesByQuery('javascript'),
-  );
+//   /*
+//    * Решил отфильтровать заметки по слову javascript
+//    */
+//   console.log(
+//     'Отфильтровали заметки по ключевому слову "javascript": ',
+//     notepad.filterNotesByQuery('javascript'),
+//   );
   
-  /*
-   * Хочу посмотреть только заметки с нормальным приоритетом
-   */
-  console.log(
-    'Отфильтровали заметки по нормальному приоритету: ',
-    notepad.filterNotesByPriority(PRIORITY_TYPES.NORMAL),
-  );
+//   /*
+//    * Хочу посмотреть только заметки с нормальным приоритетом
+//    */
+//   console.log(
+//     'Отфильтровали заметки по нормальному приоритету: ',
+//     notepad.filterNotesByPriority(PRIORITY_TYPES.NORMAL),
+//   );
   
-  /*
-   * Обновим контент заметки с id 3
-   */
-  notepad.updateNoteContent(3, { title: 'Get comfy with React.js or Vue.js' });
-  console.log(
-    'Заметки после обновления контента заметки с id 3: ',
-    notepad.getNotes(),
-  );
+//   /*
+//    * Обновим контент заметки с id 3
+//    */
+//   notepad.updateNoteContent(3, { title: 'Get comfy with React.js or Vue.js' });
+//   console.log(
+//     'Заметки после обновления контента заметки с id 3: ',
+//     notepad.getNotes(),
+//   );
   
-  /*
-   * Повторил HTML и CSS, удаляю запись c id 2
-   */
-  notepad.deleteNote(2);
-  console.log('Заметки после удаления с id 2: ', notepad.getNotes());
+//   /*
+//    * Повторил HTML и CSS, удаляю запись c id 2
+//    */
+//   notepad.deleteNote(2);
+//   console.log('Заметки после удаления с id 2: ', notepad.getNotes());
